6d51ab29ea561b06f8345c0137ca7793f6370804
Alex <alexander.salvatore@gmail.com>
2020-12-28 21:31:58 +0100

    minor change

--- src/utils/Firebase.js
+++ src/utils/Firebase.js
@@ -7,13 +7,7 @@ const settings = {
 };
 
 const config = {
-    apiKey: "AIzaSyBZYfKcY5Coy20Vk2Cjg-kzFMzEheov_iU",
-    authDomain: "labreactnative-d142b.firebaseapp.com",
-    databaseURL: "https://labreactnative-d142b.firebaseio.com",
-    projectId: "labreactnative-d142b",
-    storageBucket: "labreactnative-d142b.appspot.com",
-    messagingSenderId: "265682572620",
-    appId: "1:265682572620:web:5296c623f8c5725bceebe9"
+    /* YOUR FIREBASE CONFIG HERE */
 };
 firebase.initializeApp(config);
 firebase.firestore().settings(settings);
